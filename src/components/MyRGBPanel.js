c42ad41a3303030c2e4d1501d45372add8b24af5
github_name <johndoe@example.com>
2024-05-08 15:17:11 +0800

    1

--- src/components/MyRGBPanel.js
+++ src/components/MyRGBPanel.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import MySlider from "./MySlider";
 
 function MyRGBPanel() {
-    const [r, setR] = useState(128);
-    const [g, setG] = useState(128);
-    const [b, setB] = useState(128);
+  const [r, setR] = useState(128);
+  const [g, setG] = useState(128);
+  const [b, setB] = useState(128);
 
-    const updateR = (v) => setR(v);
-    const updateG = (v) => setG(v);
-    const updateB = (v) => setB(v);
+  const updateR = (v) => setR(v);
+  const updateG = (v) => setG(v);
+  const updateB = (v) => setB(v);
 
-    const rgbCss = {
-        border: "1px solid blue",
-        borderRadius: "20px",
-        width: "300px",
-        display: "flex",
-        flexDirection: "column",
-        padding: "20px",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "auto",
-      };
+  const rgbCss = {
+    border: "1px solid blue",
+    borderRadius: "20px",
+    width: "300px",
+    display: "flex",
+    flexDirection: "column",
+    padding: "20px",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+  };
+
+  const headingStyle = {
+    color: rgb(${r}, ${g}, ${b}),
+  };
+
   return (
     <>
-      <div
-        style={rgbCss}
-      >
-        <h2 style={{color: "red"}}>
-          目前色彩
-        </h2>
+      <div style={rgbCss}>
+        <h2 style={headingStyle}>目前色彩</h2>
         <div
           style={{
             width: "250px",
             height: "100px",
-            backgroundColor: `rgb(${r}, ${g}, ${b})`,
+            backgroundColor: rgb(${r}, ${g}, ${b}),
           }}
         />
         <span>R: </span>
